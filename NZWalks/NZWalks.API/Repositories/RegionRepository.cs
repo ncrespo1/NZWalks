56f4bef30451ea7ba7155e55717e24d477e60401
ncrespo1 <hawker20055@gmail.com>
2022-10-08 19:32:56 -0500

    New regions controller. Created regions repository. Made asynchronous.

---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext nZWalksDbContext;
+
+        public RegionRepository(NZWalksDBContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+           return await nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
